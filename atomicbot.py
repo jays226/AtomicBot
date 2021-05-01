ff94fc2c707d814cc80a86eb8237b964ae0f642a
AtomicXYZ <82727610+AtomicXYZ@users.noreply.github.com>
2021-04-30 23:04:44 -0400

    Update atomicbot.py

--- atomicbot.py
+++ atomicbot.py
@@ -908,5 +908,5 @@ async def on_message(message):
     #   return
       
       
-bot.run('ODMyMjYzNjcyODQzMDc1NjE0.YHhP8g.-XaEozpPh2QwZVQJSUkL0fsfS3I')
-# bot.run(os.environ['DISCORD_TOKEN'])
+# bot.run('ODMyMjYzNjcyODQzMDc1NjE0.YHhP8g.-XaEozpPh2QwZVQJSUkL0fsfS3I')
+bot.run(os.environ['DISCORD_TOKEN'])
